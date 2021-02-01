cf87f2b80aba1384f6faf18029e7097c64c29514
Rohan Chhabra <rohan23chhabra@gmail.com>
2021-02-01 10:10:12 +0530

    Added annotations to Course and integrated with mysql db

--- src/main/java/org/cms/core/course/Course.java
+++ src/main/java/org/cms/core/course/Course.java
@@ -1,48 +1,58 @@
 package org.cms.core.course;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import java.io.Serializable;
+import javax.persistence.*;
 
 @Entity
-public class Course {
+@Table(name = "course")
+public class Course implements Serializable {
 
-    @Id
-    private int id;
-    private String name;
-    private String description;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    public Course(){
+	private String name;
+	private String description;
 
-    }
+	public Course() {}
 
-    public Course(int id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-    }
+	public Course(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
 
+	public Course(int id, String name, String description) {
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Course{" + "id=" + id + ", name='" + name + '\'' + ", description='" + description + '\'' + '}';
+	}
 }
