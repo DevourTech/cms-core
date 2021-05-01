d029c1efcf279a9a373216b15281064a8f51b712
Archit Singh <architsingh456@gmail.com>
2021-05-01 14:52:43 +0530

    UPD : added Id Response class and updated version

---
+++ src/main/java/org/cms/core/http/IdResponse.java
@@ -0,0 +1,12 @@
+package org.cms.core.http;
+
+public class IdResponse {
+
+	String id;
+	String message;
+
+	public IdResponse(String id, String message) {
+		this.id = id;
+		this.message = message;
+	}
+}
